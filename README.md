2daa427eae2eb1943d3de87fb6d3a73d1ec5d6aa
Daffa Mumtaz <daffamumtaz2001@gmail.com>
2020-03-27 22:26:12 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Peta COVID-19 di Indonesia
+Peta penyebaran COVID-19 di Indonesia yang dibagi berdasarkan provinsi. Peta ini menampilkan jumlah kasus positif, sembuh, dan meninggal.
+## Data disediakan oleh
+- [kawalcorona.com](https://kawalcorona.com) - Data jumlah kasus per provinsi
+- [ans-4175/peta-indonesia-geojson](https://github.com/ans-4175/peta-indonesia-geojson) - Data GeoJSON provinsi di Indonesia
+- [OpenStreetMap](https://www.openstreetmap.org) - Data peta
---
+++ script.js
@@ -0,0 +1,169 @@
+var sWidth = document.documentElement.clientWidth;
+var zoomLevel;
+if(sWidth >= 1024)		zoomLevel = 5;
+else if(sWidth >= 768)	zoomLevel = 4;
+else 					zoomLevel = 3;
+
+var map = L.map('map').setView([-2.5, 118], zoomLevel);
+L.tileLayer(
+'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	maxZoom: 18,
+}).addTo(map);
+
+function loadData() {
+	Swal.fire({
+		title: 'Mohon tunggu',
+		html: 'Sedang memuat data',
+		allowOutsideClick: false,
+		allowEscapeKey: false,
+		onBeforeOpen: function() {
+			Swal.showLoading();
+		}
+	});
+	$.when(
+		$.getJSON('indonesia-prov.geojson'),
+		$.ajax({ url: 'https://api.kawalcorona.com/indonesia/provinsi/', dataType: 'json' })
+	).then(function(json, data) {
+		// ===== Process API data =====
+		var provinsi = [];
+		var total = {
+			Kasus_Posi: 0,
+			Kasus_Semb: 0,
+			Kasus_Meni: 0,
+		};
+		data[0].forEach((d) => {
+			d = d.attributes;
+			provinsi[d.Kode_Provi] = d;
+			total.Kasus_Posi += d.Kasus_Posi;
+			total.Kasus_Semb += d.Kasus_Semb;
+			total.Kasus_Meni += d.Kasus_Meni;
+		});
+
+		// ===== Map colors =====
+		var colors = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
+		var scales = [0, 5, 10, 20, 30, 50, 80, 100, 200];
+		function getColor(n) {
+			for (var i = scales.length-1; i >= 0; i--) {
+				if(n > scales[i]) {
+					return colors[i];
+				}
+			}
+		}
+
+		function style(prov) {
+			var kasus = (provinsi[prov.id] ? provinsi[prov.id].Kasus_Posi : 0);
+			return {
+				fillColor: getColor(kasus),
+				weight: 2,
+				opacity: 1,
+				color: 'white',
+				dashArray: '3',
+				fillOpacity: (kasus > 0 ? 0.7 : 0)
+			};
+		}
+
+		// ===== Event listener =====
+		// mousein event
+		function highlightFeature(e) {
+			var layer = e.target;
+
+			layer.setStyle({
+				weight: 5,
+				color: '#666',
+				dashArray: '',
+				fillOpacity: 0.7
+			});
+
+			if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+				layer.bringToFront();
+			}
+
+			info.update(layer.feature.properties);
+		}
+
+		// mouseout event
+		function resetHighlight(e) {
+			geojson.resetStyle(e.target);
+			info.update();
+		}
+
+		// click event
+		function zoomToFeature(e) {
+			map.fitBounds(e.target.getBounds());
+			info.update(e.target.feature.properties);
+		}
+
+		// attach event
+		function onEachFeature(feature, layer) {
+			layer.on({
+				mouseover: highlightFeature,
+				mouseout: resetHighlight,
+				click: zoomToFeature
+			});
+		}
+
+		// ===== Info panel =====
+		var info = L.control();
+
+		info.onAdd = function (map) {
+			this._div = L.DomUtil.create('div', 'info');
+			this.update();
+			return this._div;
+		};
+
+		info.update = function (props) {
+			var data = (props && provinsi[props.kode] ? provinsi[props.kode] : {});
+			this._div.innerHTML = '<h4>Penyebaran Kasus COVID-19</h4>' +
+				(props ?
+				'<b>' + props.Propinsi + '</b>'+
+				'<br />' + (data.Kasus_Posi || 0) + ' Positif'+
+				'<br />' + (data.Kasus_Semb || 0) + ' Sembuh'+
+				'<br />' + (data.Kasus_Meni || 0) + ' Meninggal'
+				: '<b>Total</b>'+
+				'<br />' + (total.Kasus_Posi || 0) + ' Positif'+
+				'<br />' + (total.Kasus_Semb || 0) + ' Sembuh'+
+				'<br />' + (total.Kasus_Meni || 0) + ' Meninggal');
+		};
+
+		info.addTo(map);
+
+		// ===== Legend panel =====
+		var legend = L.control({position: 'bottomright'});
+
+		legend.onAdd = function (map) {
+
+			var div = L.DomUtil.create('div', 'info legend');
+
+			for (var i = 0; i < scales.length; i++) {
+				div.innerHTML +=
+					'<i style="background:' + colors[i] + '"></i> ' +
+					scales[i] + (scales[i + 1] ? '&ndash;' + scales[i + 1] + '<br>' : '+');
+			}
+
+			return div;
+		};
+
+		legend.addTo(map);
+
+		var geojson = L.geoJson(json, {
+			style: style,
+			onEachFeature: onEachFeature,
+		}).addTo(map);
+		Swal.close();
+	}, function(data) {
+		console.log(data);
+		Swal.fire({
+			icon: 'error',
+			title: 'Ups!',
+			text: 'Terjadi kesalahan saat memuat data',
+			confirmButtonText: 'Coba Lagi',
+			allowOutsideClick: false,
+			allowEscapeKey: false,
+		}).then(() => {
+			loadData();
+		});
+	});
+}
+
+loadData();
